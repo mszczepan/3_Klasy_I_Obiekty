a0c47489646cb83de95a7f7b680d1a38aaa9e436
Mateusz <szczepan.mateusz@gmail.com>
2020-04-20 21:02:15 +0200

    3 Karta-Klasy i metody

--- 3_Klasy_I_Obiekty/3 KArtaOcennFilmow/Karta.cs
+++ 3_Klasy_I_Obiekty/3 KArtaOcennFilmow/Karta.cs
@@ -8,8 +8,57 @@ namespace _3_KartaOcenFilmow
     class Karta
     {
         //Stan (zmienne -pola)
-
+        List<float> oceny ;
         //Zachowania(metody)
+       
+        /// <summary>
+        /// Dodaje nową ocenę do listy ocen
+        /// </summary>
+        /// <param name="ocena">nowa ocena</param>
+        public void DodajOcene(float ocena) 
+        {
+            oceny.Add(ocena);
+        }
 
+        /// <summary>
+        /// Obliczanie średniej z listy ocen
+        /// </summary>
+        /// <returns>Średnia ocena</returns>
+        public float ObliczSrednia()
+        {
+            //float suma = 0;
+            //float srednia = 0;
+
+            //foreach (var ocena in oceny)
+            //{
+            //    suma += ocena;
+            //    srednia = suma / oceny.Count();
+            //}
+
+            //return srednia;
+
+            return oceny.Average();
+
+        }
+
+        /// <summary>
+        /// Znajduje najniższą ocenę
+        /// </summary>
+        /// <returns>Najniższa ocena</returns>
+        public float NajnizszaOcena()
+        {
+        
+            return oceny.Min();
+        }
+
+        /// <summary>
+        /// Oblicza najwyższą ocenę 
+        /// </summary>
+        /// <returns>Najwyższa ocena</returns>
+        public float NajwyzszaWartosc()
+        {
+            return oceny.Max();
+
+        }
     }
 }
