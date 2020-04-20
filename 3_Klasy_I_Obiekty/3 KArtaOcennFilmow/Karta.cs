1df566928289a4a8332399bd517ebae4083a22ea
Mateusz <szczepan.mateusz@gmail.com>
2020-04-20 20:37:32 +0200

    3_1 Klasa-ogólnie

---
+++ 3_Klasy_I_Obiekty/3 KArtaOcennFilmow/Karta.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _3_KartaOcenFilmow
+{
+    class Karta
+    {
+        //Stan (zmienne -pola)
+
+        //Zachowania(metody)
+
+    }
+}
---
+++ 3_Klasy_I_Obiekty/3 KArtaOcennFilmow/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _3_KartaOcenFilmow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
