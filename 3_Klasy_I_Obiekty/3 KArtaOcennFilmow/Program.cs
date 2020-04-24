c77551841474d9efc9c570cc5e6a7d522881d338
Mateusz <szczepan.mateusz@gmail.com>
2020-04-24 14:08:17 +0200

    3. Objekty cd.

--- 3_Klasy_I_Obiekty/3 KArtaOcennFilmow/Program.cs
+++ 3_Klasy_I_Obiekty/3 KArtaOcennFilmow/Program.cs
@@ -11,6 +11,7 @@ namespace _3_KartaOcenFilmow
         {
 
             Karta karta=new Karta();
+            Karta kartaUzytkownika = new Karta();
             karta.DodajOcene(6);
             karta.DodajOcene(8);
             karta.DodajOcene(3);
@@ -21,8 +22,42 @@ namespace _3_KartaOcenFilmow
             float max = karta.NajwyzszaWartosc();
             float min= karta.NajnizszaOcena();
 
+            for (; ; )
+            {
+                
+                Console.WriteLine("Podaj ocene z zakresu 1-10 ");
+                float ocena;
+                bool wynik =float.TryParse(Console.ReadLine(),out ocena);
 
-            
+                if (wynik)
+                {
+                    if (ocena == 11)
+                    {
+                        break;
+                    }
+
+                    if (ocena > 0 && ocena <= 10)
+                    {
+                        kartaUzytkownika.DodajOcene(ocena);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Liczba z poza zakresu ");
+                    }
+                }
+
+                else
+                {
+                    Console.WriteLine("To nie jest liczba ");
+                }
+           
+            }
+
+            Console.WriteLine("Srednia ocena "+ kartaUzytkownika.ObliczSrednia());
+            Console.WriteLine("Najniższa ocena to " + kartaUzytkownika.NajnizszaOcena());
+            Console.WriteLine("Najwyzsza ocena to " + kartaUzytkownika.NajwyzszaWartosc());
+
+            Console.ReadKey();
         }
     }
 }
