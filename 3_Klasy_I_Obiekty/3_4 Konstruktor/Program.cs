3bdb70d5018f2993f1813ec1f4c18e422cbb58ac
Mateusz <szczepan.mateusz@gmail.com>
2020-04-25 19:16:45 +0200

    3. Konstruktor operacje

--- 3_Klasy_I_Obiekty/3_4 Konstruktor/Program.cs
+++ 3_Klasy_I_Obiekty/3_4 Konstruktor/Program.cs
@@ -10,6 +10,10 @@ namespace _3_4_Konstruktor
     {
         static void Main(string[] args)
         {
+            var osoba1 = new Osoba();
+            var osoba2 = new Osoba("Jan","Kowal");
+            var osoba3 = new Osoba("Tomek","Kowal","Katowice");
+            var osoba4 = new Osoba("Adam", "Kowal", "Katowice","Mila 15",18);
         }
     }
 }
