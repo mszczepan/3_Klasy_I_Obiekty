39615907720da68e4680cce8ebfa0475ad30de49
Mateusz <szczepan.mateusz@gmail.com>
2020-04-26 13:40:00 +0200

    3. Klasa a Zmienna

---
+++ 3_Klasy_I_Obiekty/3_6 KlasaAZmienna/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_6_KlasaAZmienna
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Karta karta= new Karta();
+            karta.DodajOcene(3);
+            karta.DodajOcene(6);
+
+            Karta karta2 = new Karta();
+            karta2.DodajOcene(4);
+        }
+    }
+}
